a592094417e49eaa2c874746d133986c1061cab9
tutak1i20 <102325269+tutak1i20@users.noreply.github.com>
2023-09-25 09:30:16 +0200

    Add files via upload

---
+++ zadanie zelki/main.js
@@ -0,0 +1,5 @@
+// Skrypt
+// Wymagania dotyczące skryptu:
+// – wykonywany po stronie przeglądarki, wywoływany przyciskiem „Zamów”
+// – skrypt sprawdza wprowadzony numer kształtu i wypisuje w akapicie poniżej przycisku „Zamów” tekst „Zamówiłeś żelka: ”, a dalej w zależności od wprowadzonej wartości: 1 – „miś”, 2 – „żabka”, 3 – „serce”, inny – „inny kształt”, np. „Zamówiłeś żelka: miś”
+// – następnie skrypt sprawdza wartości RGB, a następnie zmienia kolor tła przycisku „Wybrany kolor” na kolor odpowiadający podanej wartości RGB
